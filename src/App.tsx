ee85049b7fe023e1cc21e8b067d2c164c39e4ecd
juliflorezg <forezjuli08@hotmail.com>
2022-11-30 17:52:11 -0500

    feat: refuerzo-react-TS, add components to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import { Contador } from "./components/Contador"
+import { ContadorConHook } from "./components/ContadorConHook"
+import { Funciones } from "./typescript/Funciones"
+import { ObjetosLiterales } from "./typescript/ObjetosLiterales"
+import { TiposBasicos } from "./typescript/TiposBasicos"
 
 const App = () => {
-  return <div className="mt-2">App</div>
+  return (
+  <div className="mt-2">
+    <h1>Introducción a React con TS</h1>
+    <hr />
+    <TiposBasicos/>
+    <ObjetosLiterales/>
+    <Funciones/>
+    <Contador/>
+    <ContadorConHook/>
+  </div>
+  )
 }
 
 export default App
