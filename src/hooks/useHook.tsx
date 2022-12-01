74f26dc832f7180d0d24b7380c368a8b16d0ed44
juliflorezg <forezjuli08@hotmail.com>
2022-12-01 08:55:03 -0500

    feat: refuerzo-react-TS, add types to custom hook

--- src/hooks/useHook.tsx
+++ src/hooks/useHook.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 
-export const useHook = () => {
-  const [valor, setValor] = useState(0)
+export const useHook = (inicial: number = 10): [number, (number: number) => void] => {
+  const [valor, setValor] = useState(inicial)
 
   const acumular = (numero: number) => {
     setValor(valor + numero)
